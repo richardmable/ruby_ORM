ae6171a8259a5df20ecff2cdf62963e78ac0f830
Richard Mable <richardbmable@gmail.com>
2015-12-22 22:20:56 -0500

    added .sql file for db startup

---
+++ ruby_ORM.rb
@@ -0,0 +1,6 @@
+# Create a User object that has several class methods:
+
+# find - takes an ID argument and finds the User with that ID, returns a user object w/ information on that user from the DB
+# where - takes a Hash argument of user attributes and finds users with those attributes, returns an array of matching User objects
+
+
---
+++ ruby_ORM_db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+	id serial PRIMARY KEY,
+	fname varchar(50),
+	lname varchar(50),
+	dateCreated timestamp
+)
